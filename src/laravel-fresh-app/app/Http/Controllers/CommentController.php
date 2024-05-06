8e555dfdcd2296dc10cc65f7c92cca39e2a1b69e
NguyenDuyNam29032002 <dn29032002@gmail.com>
2024-05-06 16:06:08 +0700

    make simple app for testing

---
+++ src/laravel-fresh-app/app/Http/Controllers/CommentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(int|string $id)
+    {
+        $a = Post::query()->find($id);
+        return Comment::where('post_id', '=', $id)->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int|string $postId, int|string $commentId)
+    {
+        $result = Comment::query()->where('post_id', '=', $postId)->where('id', '=', $commentId)->get();
+        return $result;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
